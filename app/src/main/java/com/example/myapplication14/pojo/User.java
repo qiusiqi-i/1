4d2b2c5aebd0ed9022dbcac692a56a152b76a394
siqi <1939790204@qq.com>
2020-04-14 16:41:50 +0800

    第一次提交

---
+++ app/src/main/java/com/example/myapplication14/pojo/User.java
@@ -0,0 +1,7 @@
+package com.example.myapplication14.pojo;
+
+public class User {
+
+    private String name;
+
+}
---
+++ app/src/main/java/com/example/myapplication14/service/UserService.java
@@ -0,0 +1,4 @@
+package com.example.myapplication14.service;
+
+public interface UserService {
+}
